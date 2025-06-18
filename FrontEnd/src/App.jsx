01774f4187ace6052030ff7562a253036c10298f
mythili <mythilijobofficial@gmail.com>
2025-06-18 14:34:20 +0530

    Build Issue fixed

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState, useEffect, createContext, useContext } from "react";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Login from './Components/login';
 import Login from "./Components/Login/Loing";
 import ExpenseTrackerList from "./Components/Expenses/ExpenseTrackerList";
 import AddExpenseTracker from "./Components/Expenses/AddExpesneTracker";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardLayout from "./Components/DashBoard/Dashboard";
 import Signup from "./Components/Login/Signup";
 import LayOut from "./Components/LayOut/LayOut";
 import ExpenseDashboard from "./Components/DashBoard/ExpenseDashboard";
